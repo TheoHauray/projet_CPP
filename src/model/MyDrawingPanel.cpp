1ab5baa6c39c77e42dc295fdcf79b915d59d1784
Rémy <remy.albanese2@gmail.com>
2022-06-13 14:22:42 +0200

    classe MyDrawingPanel

---
+++ src/model/MyDrawingPanel.cpp
@@ -0,0 +1,28 @@
+#include "MyDrawingPanel.hpp"
+
+#include "wx/wxprec.h"
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include<wx/filedlg.h>
+#include <wx/image.h>
+#include <wx/file.h>
+#include <wx/bitmap.h>
+
+class MyDrawingPanel: public wxPanel
+
+{
+public:
+	MyDrawingPanel( wxWindow *parent ) ;
+	void OpenFile(wxString fileName) ;
+	void SaveFile(wxString fileName) ;
+
+private:
+	void OnMouseMove(wxMouseEvent &event) ;
+	void OnMouseLeftDown(wxMouseEvent &event) ;
+	void OnPaint(wxPaintEvent &event) ;
+	wxPoint m_mousePoint ;
+	wxPoint m_onePoint ;
+};
