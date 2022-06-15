a59e7575ad7040f9645c7d70c4d685b4601ebfce
TheoHauray <theohauray44@gmail.com>
2022-06-15 14:52:51 +0200

    modif error point - rect

--- src/model/Point.cpp
+++ src/model/Point.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 Point::Point()
 {
-    m_label = "Point"
+    m_label = "Point";
     m_x = 0;
     m_y = 0;
 
@@ -19,7 +19,7 @@ Point::Point()
 
 Point::Point(int xy)
 {
-    m_label = "Point"
+    m_label = "Point";
     m_x = xy;
     m_y = xy;
 
@@ -27,6 +27,7 @@ Point::Point(int xy)
 
 Point::Point(int x, int y)
 {
+    m_label = "Point";
     m_x = x;
     m_y = y;
 
@@ -34,6 +35,7 @@ Point::Point(int x, int y)
 
 Point::Point(const Point& point)
 {
+    m_label = "Point";
     m_x = point.m_x;
     m_y = point.m_y;
 
@@ -110,5 +112,5 @@ float Point::distance(const Point& point) const
 
 void Point::draw(wxDC* dc)
 {
-    
+
 }
