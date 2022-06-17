edee595fbe0a933b5e937637612ee7d2f17902f2
Rémy <remy.albanese2@gmail.com>
2022-06-17 14:28:45 +0200

    version finale

--- src/MyApp.cpp
+++ src/MyApp.cpp
@@ -21,7 +21,7 @@
 #define WIDGET_PANEL_WIDTH	150
 #define WIDGET_Y0			30
 #define WIDGET_Y_STEP		50
-#define APP_NAME "M1102 Skeleton 1.0"
+#define APP_NAME "Dessin vectoriel"
 
 
 //------------------------------------------------------------------------
--- src/view/Constant.hpp
+++ src/view/Constant.hpp
@@ -6,4 +6,4 @@
 #define WIDGET_PANEL_WIDTH	150
 #define WIDGET_Y0			30
 #define WIDGET_Y_STEP		50
-#define APP_NAME "M1102 Skeleton 1.0"
+#define APP_NAME "Dessin vectoriel"
